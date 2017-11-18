d3e20c9957e59784c1a02c4bf06a088268501597
Alex Lee <alee490@my.bcit.ca>
2017-11-18 08:46:44 -0800

    Added Alex project

---
+++ projects/projects_alex.php
@@ -0,0 +1,13 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/header.php');
+?>
+<div class="info">
+        <section class="team-projects">
+            <h1 class="text-center">Team Projects</h1>
+        </section>
+        <section class="alex-projects">
+            <h1 class="text-center">Alex's Projects</h1>
+        </section>
+    </div>
+
+    <?include($_SERVER['DOCUMENT_ROOT'] .'/footer.php');?>
