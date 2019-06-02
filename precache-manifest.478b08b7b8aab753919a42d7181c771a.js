00936c438d4990fd0a186be34d19b7abc003b383
Henry Ho <henrychho89@gmail.com>
2019-06-01 18:16:29 -0700

    Updates

---
+++ precache-manifest.478b08b7b8aab753919a42d7181c771a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f2bfe38ce77fdbb93e4a",
+    "url": "/static/js/main.e5b4f74c.chunk.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "f2bfe38ce77fdbb93e4a",
+    "url": "/static/css/main.ce39f52b.chunk.css"
+  },
+  {
+    "revision": "e3649b3cd20de19548fd7ea612229953",
+    "url": "/index.html"
+  }
+];
